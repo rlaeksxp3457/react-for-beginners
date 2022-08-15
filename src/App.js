8d4256450ca2a14d3ad97e5c2258839f8dbd3ac0
Elite <rlaeksxp3457@gmail.com>
2022-08-15 20:09:18 +0900

    리액트 공부
    To-Do-List 만들기,
    useEffect 다루기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+// import styles from "./App.module.css";
+// import { useState, useEffect } from "react";
+import { useState } from "react";
+/*
+* 기초학습
+*
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
 
-function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+function Showing() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+    </div>
+  );
+}
+* function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
@@ -27,12 +55,49 @@ function App() {
 
   useEffect(() => {
     console.log("I run when 'keyword and counter' change.");
-  }, [keyword,counter])
+  }, [keyword, counter]);
+
   return (
     <div>
       <input onChange={onChange} type="text" placeholder="Search here..." />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      <Showing />
+    </div>
+  );
+}
+ */
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = ({ target: target }) => setToDo(target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo !== "") {
+      setToDos((currentArray) => [toDo,...currentArray]);
+      setToDo("");
+    }
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, idx) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
