5986bf966e95435e15209cc5ec7bf5cd110ceb3f
Elite <rlaeksxp3457@gmail.com>
2022-08-15 18:05:00 +0900

    리액트 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(`SEARCH FOR ${keyword}`);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' change.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' change.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword and counter' change.");
+  }, [keyword,counter])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={onChange} type="text" placeholder="Search here..." />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
