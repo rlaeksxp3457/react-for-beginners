7f86848c71717c63baca2183c206d1491e39c850
Elite <rlaeksxp3457@gmail.com>
2022-08-20 02:31:27 +0900

    리액트 공부 movie 컴포넌트 생성
    Detail 컴포넌트 생성
    
    App 라우트 처리

--- src/App.js
+++ src/App.js
@@ -1,104 +1,15 @@
-// import styles from "./App.module.css";
-// import { useState, useEffect } from "react";
-import { useState } from "react";
-/*
-* 기초학습
-*
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-}
-
-function Showing() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
-  };
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
-    </div>
-  );
-}
-* function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("i run only once");
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log(`SEARCH FOR ${keyword}`);
-    }
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' change.");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' change.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run when 'keyword and counter' change.");
-  }, [keyword, counter]);
-
-  return (
-    <div>
-      <input onChange={onChange} type="text" placeholder="Search here..." />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      <Showing />
-    </div>
-  );
-}
- */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = ({ target: target }) => setToDo(target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo !== "") {
-      setToDos((currentArray) => [toDo,...currentArray]);
-      setToDo("");
-    }
-  };
-  console.log(toDos);
   return (
-    <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <Link to="/movie">
+        <h2>{title}</h2>
+      </Link>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.prototypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
